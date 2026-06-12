4d7a12a1b55a6984bc710374a55a01e09a6641ef
SaiPavan <70737500+SaiPavanNikhil@users.noreply.github.com>
2026-06-12 18:23:42 +0530

    wswss
    ws

--- src/app/pages/exam/exam.component.ts
+++ src/app/pages/exam/exam.component.ts
@@ -530,7 +530,7 @@ async launchHardwareAndMediaPipelines() {
 
   // this.socket = new WebSocket('ws://127.0.0.1:8000/ws/audio');
 this.socket = new WebSocket(
-  'wss://ai-exam-backend-code-production.up.railway.app/ws/audio'
+  'ws://ai-exam-backend-code-production.up.railway.app/ws/audio'
 );
   this.socket.onopen = () => {
     console.log('✅ WebSocket Connected');
