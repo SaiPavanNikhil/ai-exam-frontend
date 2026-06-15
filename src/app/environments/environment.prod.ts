68102edcff04cb57c1e4bc447ee89a7eeda60e2d
sai pavan <221910307037@gitam.in>
2026-06-15 15:04:31 +0530

    update prod environment

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -4,5 +4,6 @@ console.log("origin:", window.location.origin);
 
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://ai-exam-backend-code-production.up.railway.app'
+  // apiBaseUrl: 'https://ai-exam-backend-code-production.up.railway.app'
+  apiBaseUrl: 'http://localhost:8000'
 };
