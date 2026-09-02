b3c0fd427c87fb1d26bfed9820e8adc9fe14766c
unknown <221910307037@gitam.in>
2026-09-02 17:34:46 +0530

    environments change

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -4,6 +4,6 @@ console.log("origin:", window.location.origin);
 
 export const environment = {
   production: true,
-  // apiBaseUrl: 'https://ai-exam-backend-code-production.up.railway.app'
-  apiBaseUrl: 'http://localhost:8000'
+  apiBaseUrl: 'https://ai-exam-backend-code-production.up.railway.app'
+  // apiBaseUrl: 'http://localhost:8000'
 };
